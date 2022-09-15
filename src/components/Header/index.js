07e52a156483aaee1d6877eb1d3ac7cd4a6ba0ce
Scott Nicholson <scott@Scotts-MacBook-Air-2.local>
2022-09-15 22:16:19 +0100

    fix: fixing url header links for gh-pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header>
-      <Link to="/" className="header-title homepage-link">
+      <Link to="/charlie-minton/" className="header-title homepage-link">
         <img src="assets/logo.svg" alt="logo" />
         {/* <h1>Charlie Minton</h1> */}
       </Link>
       <div className="header-links">
-        <Link to="/gallery" className="homepage-link">
+        <Link to="/charlie-minton/gallery" className="homepage-link">
           Gallery
         </Link>
-        <Link to="/basket" className="homepage-link">
+        <Link to="/charlie-minton/basket" className="homepage-link">
           Basket
         </Link>
         {/* <Link to="/about" className="homepage-link">
