4bff4e61e5dbe732a4163190c1278c68ffd320dd
Scott Nicholson <scott@Scotts-MacBook-Air-2.local>
2022-09-26 20:49:35 +0100

    fix: fixing header logo not loading on deeper pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 import { Link } from "react-router-dom";
+import logo from "./logo.svg";
 
 export const Header = () => {
   return (
     <header>
       <Link to="/charlie-minton/" className="header-title homepage-link">
-        <img src="assets/logo.svg" alt="logo" />
-        {/* <h1>Charlie Minton</h1> */}
+        <img src={logo} alt="logo" />
       </Link>
       <div className="header-links">
         <Link to="/charlie-minton/gallery" className="homepage-link">
